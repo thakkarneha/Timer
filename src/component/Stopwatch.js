f44fdcf2e3f2e2bf62c4300c017e1121ad388768
Neha Thakkar <112370511+thakkarneha@users.noreply.github.com>
2022-11-07 20:52:45 +0530

    updated stopwatch.js

--- src/component/Stopwatch.js
+++ src/component/Stopwatch.js
@@ -15,14 +15,26 @@ function Stopwatch() {
 
     const [mss,setmss]=useState(milisceonds)
 
+const[disable,setDisable]=useState(false)
+const[disable1,setDisable1]=useState(true)
 
 const reset=()=>{
     sethh(0)
     setmm(0)
     setss(0)
     setmss(0)
-}
+    setDisable(true)
+    setDisable1(false)
 
+}
+const start=()=>{
+  setDisable1(true)
+  setDisable(false)
+ sethh(hour)
+  setmm(minutes)
+    setss(seconds)
+    setmss(milisceonds)
+}
 
 
 
@@ -30,7 +42,8 @@ const reset=()=>{
     <>
     <div className='container'>Stopwatch
     <h1>{hh}:{mm}:{ss}:{mss}</h1>
-    <button className="btn btn-danger btn-circle btn-sm" onClick={reset}>Reset</button>
+    <button className="btn btn-danger btn-circle btn-sm" onClick={start} disabled={disable1}>Start</button>
+    <button className="btn btn-danger btn-circle btn-sm" onClick={reset} disabled={disable}>Reset</button>
 
     </div>
     </>
