d7dca2b66da9cb6c589495728816ae886c41b61c
Neha Thakkar <112370511+thakkarneha@users.noreply.github.com>
2022-11-08 08:19:24 +0530

    updated

--- src/component/Stopwatch.js
+++ src/component/Stopwatch.js
@@ -1,51 +1,58 @@
-import React, {  useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 
 function Stopwatch() {
-    const time = new Date();
-    var hour=time.getHours();
-    var minutes=time.getMinutes();
-    var seconds=time.getSeconds();
-   var milisceonds= time.getMilliseconds();
+
+  const [hh, sethh] = useState(0)
+  const [mm, setmm] = useState(0)
+
+  const [ss, setss] = useState(0)
+
+  const [mss, setmss] = useState(0)
+
+ 
+  var stopwatch;
+ useEffect(()=>{
+ 
+  stopwatch= setInterval(()=>{
+    setmss(mss+1);
+    if(mss===999){
+      setss(ss+1);
+      setmss(0);
+    } if(ss===60){
+      setmm(mm+1);
+      setss(0);
+    }
+    if(mm===60){
+      sethh(hh+1);
+      setmm(0);
+    }
+  },1000)
+  return ()=>clearInterval(stopwatch);
+ })
+  const reset = () => {
+    sethh(0);
+    setmm(0);
+    setss(0);
+    setmss(0);
    
-    const [hh,sethh]=useState(hour)
-    const [mm,setmm]=useState(minutes)
 
-    const [ss,setss]=useState(seconds)
-
-    const [mss,setmss]=useState(milisceonds)
-
-const[disable,setDisable]=useState(false)
-const[disable1,setDisable1]=useState(true)
-
-const reset=()=>{
-    sethh(0)
-    setmm(0)
-    setss(0)
-    setmss(0)
-    setDisable(true)
-    setDisable1(false)
-
-}
-const start=()=>{
-  setDisable1(true)
-  setDisable(false)
- sethh(hour)
-  setmm(minutes)
-    setss(seconds)
-    setmss(milisceonds)
-}
+  }
+  const stop = () => {
+    clearInterval(stopwatch);
+   
+  }
 
 
 
   return (
     <>
-    <div className='container'>Stopwatch
-    <h1>{hh}:{mm}:{ss}:{mss}</h1>
-    <button className="btn btn-danger btn-circle btn-sm" onClick={start} disabled={disable1}>Start</button>
-    <button className="btn btn-danger btn-circle btn-sm" onClick={reset} disabled={disable}>Reset</button>
+      <div className='container'>Stopwatch
+        <h1>{hh}:{mm}:{ss}:{mss}</h1>
+        <button className="btn btn-danger btn-circle btn-sm" onClick={stop} >Stop</button>
+        <button className="btn btn-danger btn-circle btn-sm" onClick={reset} >Restart</button>
 
-    </div>
+      </div>
     </>
   )
 }
